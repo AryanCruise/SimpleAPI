a202d6b2db5dcbfc67e2731cc8e18311a639bb9a
Aryan Nair <work23aryan@gmail.com>
2025-01-30 13:04:25 +0530

    Updated main.go file with .env file

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
+
 import (
-	"github.com/gin-gonic/gin"
+	"go_projects/config"
 	"go_projects/routes"
+	"github.com/gin-gonic/gin"
 )
 
 func main(){
 	router:= gin.Default()
 	routes.SetupRoutes(router)
-	router.Run("0.0.0.0:3030")
+	port := config.GetEnv("PORT", "8080")
+	router.Run("0.0.0.0:" + port)
 }
